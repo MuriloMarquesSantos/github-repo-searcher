1f8d138accf2563d56339ce706477e461cb6e211
MuriloMarquesSantos <murilommms@gmail.com>
2021-02-13 14:28:00 -0300

    Finishing styling first components of dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,18 +2,46 @@ import React from 'react';
 
 import { Title, Form, Repositories } from './styles';
 import logoImg from '../../assets/github-logo.svg';
+import { FiChevronRight } from 'react-icons/fi';
 
 const Dashboard: React.FC = () => (
     <>
         <img src={logoImg} alt="Github Explorer" />
         <Title>Explore Repositórios no Github</Title>
         <Form>
-            <input placeholder="Digite o nome do repositório"/>
+            <input placeholder="Digite o nome do repositório" />
             <button type="submit">Pesquisar</button>
         </Form>
         <Repositories>
             <a href="teste">
-
+                <img src="https://avatars.githubusercontent.com/u/18178861?s=460&u=59603cbfc344014c40f13a1d08cc6da1e1c927a9&v=4"
+                    alt="Murilo"
+                />
+                <div>
+                    <strong>murilo-repo</strong>
+                    <p>Esse repo é tal</p>
+                </div>
+                <FiChevronRight size={20}/>
+            </a>
+            <a href="teste">
+                <img src="https://avatars.githubusercontent.com/u/18178861?s=460&u=59603cbfc344014c40f13a1d08cc6da1e1c927a9&v=4"
+                    alt="Murilo"
+                />
+                <div>
+                    <strong>murilo-repo</strong>
+                    <p>Esse repo é tal</p>
+                </div>
+                <FiChevronRight size={20}/>
+            </a>
+            <a href="teste">
+                <img src="https://avatars.githubusercontent.com/u/18178861?s=460&u=59603cbfc344014c40f13a1d08cc6da1e1c927a9&v=4"
+                    alt="Murilo"
+                />
+                <div>
+                    <strong>murilo-repo</strong>
+                    <p>Esse repo é tal</p>
+                </div>
+                <FiChevronRight size={20}/>
             </a>
         </Repositories>
     </>
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -44,5 +44,53 @@ export const Form = styled.form`
 `
 
 export const Repositories = styled.div`
+    margin-top: 80px;
+    max-width: 700px;
 
+    a {
+        background: #FFF;
+        border-radius: 5px;
+        width: 100%;
+        padding: 24px;
+        display: block;
+        text-decoration: none;
+
+        display: flex;
+        align-items: center;
+        transition: transform 0.2s;
+
+        &:hover {
+            transform: translateX(7px)
+        }
+
+        /* second element onwards*/
+        & + a {
+            margin-top: 16px;
+        }
+
+        img {
+            width: 64px;
+            height: 64px;
+            border-radius: 50%;
+        }
+
+        div {
+            margin-left: 16px;
+
+            strong {
+                font-size: 20px;
+                color: #3D3D4D;
+            }
+
+            p {
+                font-size: 18px;
+                color: #A8A8B3;
+                margin-top: 4px;
+            }
+        }
+        svg {
+            margin-left: auto;
+            color: #cbcbd6;
+        }
+    }
 `
